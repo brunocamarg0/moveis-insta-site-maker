10db69f75c0f7d38bae1cb21c9a651462cc83bcb
Bruno <brunocamargocontato@hotmail.com>
2025-07-09 18:25:23 -0300

    Adiciona botão flutuante do WhatsApp com logo original

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,6 +6,7 @@ import Projects from "@/components/Projects";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import WhatsAppButton from "@/components/WhatsAppButton";
 
 const Index = () => {
   return (
@@ -17,6 +18,7 @@ const Index = () => {
       <About />
       <Contact />
       <Footer />
+      <WhatsAppButton />
     </div>
   );
 };
